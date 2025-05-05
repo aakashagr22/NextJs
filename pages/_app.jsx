eaea959b2be78fdac289657f019c7c1bb4f34c90
Akash Agrahari <akashkumargupta9918@gmail.com>
2025-05-05 17:25:36 +0530

    First Update

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,10 @@
-// import "@/app/globals.css"
+import React from 'react'
 import "../src/app/globals.css";
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+export default MyApp
+
+
